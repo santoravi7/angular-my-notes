b72d380f42449b798bf5df857aca7fa4758d13af
santoravi7 <santoravi7@gmail.com>
2020-03-04 00:54:25 +0530

    4 files modified

--- src/app/notes.ts
+++ src/app/notes.ts
@@ -1,14 +1,17 @@
 export const notes = [
   {
     name: 'Note 1',    
-    description: 'this is my first note description. I can add notes and to-do list here and save it for my future reference!!!'
+    description: 'this is my first note description. I can add notes and to-do list here and save it for my future reference!!!',
+    color: '#602379'
   },
   {
     name: 'Note 2',    
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    color: '#29b38d'
   },
   {
     name: 'Note 3',    
-    description: ''
+    description: '',
+    color: '#f3785c'
   }
 ];
